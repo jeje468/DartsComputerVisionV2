887e51d99287f768ae4589f75c4ba21f05544786
Mate Bene <mate.bene0502@gmail.com>
2022-12-14 20:39:15 +0100

    Webcam test

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import cv2
 
 st.set_page_config(page_title="Main Menu")
 
 st.title("Computer vision darts game")
+
+vid = cv2.VideoCapture(0)
+FRAME_WINDOW = st.image([])
+
+while(True):
+      
+    ret, frame = vid.read()
+    
+    FRAME_WINDOW.image(frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+vid.release()
